becd70ae7fa377985f5ed6b93d9de25c5e52345e
aklezovics <arturs.klezovics@raisin.com>
2021-05-09 19:12:38 +0200

    Initial commit

---
+++ src/test/java/com/klezovich/designpatterns/DesignpatternsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.klezovich.designpatterns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignpatternsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
